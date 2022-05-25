0e6d9828a19282ef2d6ee91586296ba2cada0821
Nicolas <nicolasdewae@protonmail.com>
2022-05-25 15:49:51 +0200

    feat(program.cs): add builder options

--- doItForMeBack/Program.cs
+++ doItForMeBack/Program.cs
@@ -1,3 +1,8 @@
+using doItForMeBack.Data;
+using doItForMeBack.Service;
+using doItForMeBack.Services;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +11,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddScoped<IUserService, UserService>();
+// La méthode "UseSqlServer" récupère la chaine de connexion pour se connecter à la base de données
+var connectionString = builder.Configuration.GetConnectionString("DoItForMeDatabase");
+builder.Services.AddDbContext<DataContext>(o =>
+{
+    o.UseSqlServer(connectionString);
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
