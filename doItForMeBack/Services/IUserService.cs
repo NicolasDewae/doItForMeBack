cd0da6ba80e0b0a2623ef344d950e5c677c14159
Nicolas De waegenaere <de_waegenaere@live.fr>
2022-06-15 09:39:01 +0200

    feat(services): login and token generator

--- doItForMeBack/Services/IUserService.cs
+++ doItForMeBack/Services/IUserService.cs
@@ -1,11 +1,14 @@
-﻿using doItForMeBack.Models;
+﻿using doItForMeBack.Entities;
+using doItForMeBack.Models;
 
 namespace doItForMeBack.Service;
 
 public interface IUserService
 {
+    AuthenticateResponse Authenticate(AuthenticateRequest model);
     bool CreateUser(User user);
     IQueryable<User> GetUsers();
+    User GetUserById(int id);
     bool UserExists(int userId);
     bool Save();
 }
--- doItForMeBack/Services/UserService.cs
+++ doItForMeBack/Services/UserService.cs
@@ -1,17 +1,57 @@
 ﻿using doItForMeBack.Data;
+using doItForMeBack.Entities;
+using doItForMeBack.Helpers;
 using doItForMeBack.Models;
 using doItForMeBack.Service;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace doItForMeBack.Services
 {
     public class UserService: IUserService
     {
+        private readonly AppSettings _appSettings;
         private readonly DataContext _db;
 
-        public UserService(DataContext db)
+        public UserService(DataContext db, IOptions<AppSettings> appSettings)
         {
             _db = db;
+            _appSettings = appSettings.Value;
         }
+
+        public AuthenticateResponse Authenticate(AuthenticateRequest model)
+        {
+            var user = _db.Users.SingleOrDefault(x => x.Email == model.Email && x.Password == model.Password);
+
+            if(user == null)
+            {
+                return null;
+            }
+
+            var token = GenerateJwtToken(user);
+
+            return new AuthenticateResponse(user, token);
+
+        }
+
+        public string GenerateJwtToken(User user)
+        {
+            // generate token that is valid for 7 days
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_appSettings.Secret);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[] { new Claim("id", user.Id.ToString()) }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+
         /// <summary>
         /// Service qui ajoute un utilisateur en base de données
         /// </summary>
@@ -30,6 +70,12 @@ namespace doItForMeBack.Services
         {
             return _db.Users.AsQueryable();
         }
+
+        public User GetUserById(int id)
+        {
+            return _db.Users.FirstOrDefault(x => x.Id == id);
+        }
+
         /// <summary>
         /// Service qui vérifie en base de données si l'utilisateur existe
         /// </summary>
