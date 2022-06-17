5234276675b1f5dd05163ea4b7696c3f0e527112
Nicolas De waegenaere <de_waegenaere@live.fr>
2022-06-17 14:38:56 +0200

    feat(services): add security interface and class

---
+++ doItForMeBack/Services/ISecurityService.cs
@@ -0,0 +1,10 @@
+﻿using doItForMeBack.Entities;
+using doItForMeBack.Models;
+
+namespace doItForMeBack.Services;
+
+public interface ISecurityService
+{
+    AuthenticateResponse login(AuthenticateRequest model);
+}
+
