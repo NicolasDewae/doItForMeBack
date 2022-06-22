d8a94ef4207bfd5732f8a71278c0caf137bdf335
Nicolas De waegenaere <de_waegenaere@live.fr>
2022-06-22 15:08:47 +0200

    refactor(controller): suppression d'adminController et ajout de régions dans UserController

--- doItForMeBack/Controllers/UserController.cs
+++ doItForMeBack/Controllers/UserController.cs
@@ -9,7 +9,6 @@ namespace doItForMeBack.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    [Authorize(Roles = "Admin, User")]
     public class UserController : ControllerBase
     {
         private readonly IUserService _userService;
@@ -18,11 +17,14 @@ namespace doItForMeBack.Controllers
             _userService = userService;
         }
 
+        #region User
+        
         /// <summary>
         /// Permet de récupérer les informations de l'utilisateur existant
         /// </summary>
         /// <returns></returns>
         [HttpGet("currentUser")]
+        [Authorize(Roles = "Admin, User")]
         public IActionResult currentUser()
         {
             var currentUser = (User)HttpContext.Items["User"];
@@ -34,5 +36,38 @@ namespace doItForMeBack.Controllers
 
             return Ok(currentUser);
         }
+        #endregion
+
+        #region admin
+
+        /// <summary>
+        /// Permet de récupérer un utilisateur selon l'id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("GetUserById")]
+        [Authorize(Roles = "Admin")]
+        public IActionResult GetUserById(int id)
+        {
+            if (_userService.GetUserById(id) == null)
+            {
+                return BadRequest();
+            }
+
+            return Ok(_userService.GetUserById(id));
+        }
+
+        /// <summary>
+        /// Permet de récupérer tous les utilisateurs et leurs attributs
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("GetUsers")]
+        [Authorize(Roles = "Admin")]
+        public IQueryable GetUsers()
+        {
+            return _userService.GetUsers();
+        }
+
+        #endregion
     }
 }
