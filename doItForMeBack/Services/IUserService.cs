cc7a786e4cabb008f47e04972ede6f0284b735eb
Nicolas De waegenaere <de_waegenaere@live.fr>
2022-06-24 16:57:06 +0200

    feat(service): updateUser + deleteUser

--- doItForMeBack/Services/IUserService.cs
+++ doItForMeBack/Services/IUserService.cs
@@ -7,8 +7,10 @@ public interface IUserService
 {
     bool CreateUser(User user);
     bool UpdatePassword(int userId, string oldPassword, string newPassword);
+    bool UpdateUser(User user);
     IQueryable<User> GetUsers();
     User GetUserById(int id);
     bool UserExists(int userId);
     bool Save();
+    bool DeleteUser(User user);
 }
