7271b9dff62a7cee331e1baad9b7b9ff96948ae8
Nicolas De waegenaere <de_waegenaere@live.fr>
2022-07-20 15:20:59 +0200

    feat(entity): add Ban entity

---
+++ doItForMeBack/Entities/Ban.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace doItForMeBack.Entities
+{
+    public class Ban
+    {
+        #region Properties
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public User UserBan { get; set; }
+        [Required]
+        public DateTime BanDate { get; set; }
+        public bool IsBan { get; set; }
+        public string Description { get; set; }
+        #endregion
+    }
+}
