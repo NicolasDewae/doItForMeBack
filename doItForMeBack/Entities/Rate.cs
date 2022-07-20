8a264f6a4e8dbbb804318ff99c8733961e6840a7
Nicolas De waegenaere <de_waegenaere@live.fr>
2022-07-20 15:21:20 +0200

    feat(entity): add Rate entity

---
+++ doItForMeBack/Entities/Rate.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace doItForMeBack.Entities
+{
+    public class Rate
+    {
+        #region Properties
+        [Key]
+        public int Id { get; set; }
+        public User UserRate { get; set; }
+        public User UserRated { get; set; }
+        public float Star { get; set; }
+        #endregion
+    }
+}
+
