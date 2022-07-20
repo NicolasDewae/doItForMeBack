d1573bd1fdf9ec7fbd8fa49b01344e92a2e29875
Nicolas De waegenaere <de_waegenaere@live.fr>
2022-07-20 15:20:06 +0200

    feat(migration): add new migrations

---
+++ doItForMeBack/Migrations/20220719141705_add_rate.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace doItForMeBack.Migrations
+{
+    public partial class add_rate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Rate",
+                table: "Users");
+
+            migrationBuilder.AddColumn<int>(
+                name: "RateId",
+                table: "Users",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Rate",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    IdUserRate = table.Column<int>(type: "int", nullable: false),
+                    IdUserRated = table.Column<int>(type: "int", nullable: false),
+                    star = table.Column<float>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Rate", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_RateId",
+                table: "Users",
+                column: "RateId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Rate_RateId",
+                table: "Users",
+                column: "RateId",
+                principalTable: "Rate",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Rate_RateId",
+                table: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Rate");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Users_RateId",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "RateId",
+                table: "Users");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Rate",
+                table: "Users",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
---
+++ doItForMeBack/Migrations/20220719151016_add_Ban.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace doItForMeBack.Migrations
+{
+    public partial class add_Ban : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Rate_Users_UserId",
+                table: "Rate");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Rate",
+                table: "Rate");
+
+            migrationBuilder.RenameTable(
+                name: "Rate",
+                newName: "rates");
+
+            migrationBuilder.RenameColumn(
+                name: "star",
+                table: "rates",
+                newName: "Star");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Rate_UserId",
+                table: "rates",
+                newName: "IX_rates_UserId");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Ban",
+                table: "Users",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_rates",
+                table: "rates",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_rates_Users_UserId",
+                table: "rates",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_rates_Users_UserId",
+                table: "rates");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_rates",
+                table: "rates");
+
+            migrationBuilder.DropColumn(
+                name: "Ban",
+                table: "Users");
+
+            migrationBuilder.RenameTable(
+                name: "rates",
+                newName: "Rate");
+
+            migrationBuilder.RenameColumn(
+                name: "Star",
+                table: "Rate",
+                newName: "star");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_rates_UserId",
+                table: "Rate",
+                newName: "IX_Rate_UserId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Rate",
+                table: "Rate",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Rate_Users_UserId",
+                table: "Rate",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+    }
+}
