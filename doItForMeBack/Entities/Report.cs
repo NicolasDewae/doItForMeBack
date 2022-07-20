abe5981c36042e6e8376ebe259cfcbb14ec75c4d
Nicolas De waegenaere <de_waegenaere@live.fr>
2022-07-20 15:21:33 +0200

    feat(entity): add Report entity

---
+++ doItForMeBack/Entities/Report.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace doItForMeBack.Entities
+{
+    public class Report
+    {
+        #region Properties
+        [Key]
+        public int Id { get; set; }
+        public User User { get; set; }
+        public Mission Mission { get; set; }
+        public string Description { get; set; }
+        public string Picture { get; set; }
+        public string Subject { get; set; }
+        public DateTime CreationDate { get; set; }
+        #endregion
+    }
+}
