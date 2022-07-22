dd04f5f1465ff177632e158b85b3281e85dcee30
Nicolas De waegenaere <de_waegenaere@live.fr>
2022-07-22 17:22:12 +0200

    feat(entities): Mission

--- doItForMeBack/Entities/Report.cs
+++ doItForMeBack/Entities/Report.cs
@@ -7,10 +7,10 @@ namespace doItForMeBack.Entities
         #region Properties
         [Key]
         public int Id { get; set; }
-        public User User { get; set; }
-        public Mission Mission { get; set; }
+        public User? User { get; set; }
+        public Mission? Mission { get; set; }
         public string Description { get; set; }
-        public string Picture { get; set; }
+        public string? Picture { get; set; }
         public string Subject { get; set; }
         public DateTime CreationDate { get; set; }
         #endregion
