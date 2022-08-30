650aa8d358b7774c9536f3f3576f748204e73a12
Nicolas De waegenaere <de_waegenaere@live.fr>
2022-08-30 16:48:53 +0200

    feat(entities): report entity created

--- doItForMeBack/Entities/Report.cs
+++ doItForMeBack/Entities/Report.cs
@@ -7,12 +7,14 @@ namespace doItForMeBack.Entities
         #region Properties
         [Key]
         public int Id { get; set; }
-        public User? User { get; set; }
-        public Mission? Mission { get; set; }
+        public int ClaimantId { get; set; }
+        public int? UserId { get; set; }
+        public int? MissionId { get; set; }
         public string Description { get; set; }
         public string? Picture { get; set; }
         public string Subject { get; set; }
         public DateTime CreationDate { get; set; }
+        public DateTime? LastUpdate { get; set; }
         #endregion
     }
 }
