a10900861dd5e6039c62d8a0c17290091a3e69c5
Nicolas De waegenaere <de_waegenaere@live.fr>
2022-08-30 16:45:31 +0200

    feat(interface): interface report created

---
+++ doItForMeBack/Services/Interfaces/IReportService.cs
@@ -0,0 +1,15 @@
+﻿using doItForMeBack.Entities;
+
+namespace doItForMeBack.Services.Interfaces
+{
+    public interface IReportService
+    {
+        IQueryable<Report> GetAllReports();
+        Report GetReportById(int id);
+        bool CreateReport(Report report);
+        bool UpdateReport(Report report);
+        bool DeleteReport(Report report);
+        bool ReportExists(int reportId);
+        bool Save();
+    }
+}
