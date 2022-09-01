0e4b6403a345296bb40517b923a3312b92cd38c9
Nicolas De waegenaere <de_waegenaere@live.fr>
2022-09-01 15:26:14 +0200

    feat(model): request models added for cleaned Json

---
+++ doItForMeBack/Models/BanRequest.cs
@@ -0,0 +1,14 @@
+﻿using doItForMeBack.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace doItForMeBack.Models
+{
+    public class BanRequest
+    {
+        [Required]
+        public int WhoIsBannedId { get; set; }
+        [Required]
+        public bool IsBan { get; set; }
+        public string? Description { get; set; }
+    }
+}
---
+++ doItForMeBack/Models/UserRequest.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace doItForMeBack.Models
+{
+    public class UserRequest
+    {
+        public string Firstname { get; set; }
+        [Required]
+        public string Lastname { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Role { get; set; }
+        [Required]
+        public string Adress { get; set; }
+        [Required]
+        public string PostCode { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        public string State { get; set; }
+        [Required]
+        public DateTime Birthday { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Picture { get; set; }
+    }
+}
