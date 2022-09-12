b36ae5de445500ffebb5e8abf0fbab4635e708a0
Nicolas De waegenaere <de_waegenaere@live.fr>
2022-09-12 17:39:34 +0200

    feat(service): mission request

--- doItForMeBack/Services/Interfaces/IMissionService.cs
+++ doItForMeBack/Services/Interfaces/IMissionService.cs
@@ -1,14 +1,14 @@
 ﻿using doItForMeBack.Entities;
+using doItForMeBack.Models;
 
 namespace doItForMeBack.Services.Interfaces
 {
     public interface IMissionService
     {
-        bool CreateMission(Mission mission);
+        bool CreateMission(MissionRequest mission, User currentUser);
         bool UpdateMission(Mission mission);
         IQueryable<Mission> GetMissions();
         Mission GetMissionById(int id);
-        IQueryable<Mission> GetBanMissions();
         bool DeleteMission(Mission mission);
         bool MissionExists(int missionId);
         bool Save();
